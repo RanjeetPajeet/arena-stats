882edcccc4b09a9f2d295b3400bf97f82739f7cf
RanjeetPajeet <68920468+RanjeetPajeet@users.noreply.github.com>
2023-01-03 00:19:03 -0600

    Update data.py

--- data.py
+++ data.py
@@ -212,7 +212,7 @@ def get_2v2_winrates(data_2v2: pd.DataFrame) -> pd.DataFrame:
     data_2v2['win'] = data_2v2['win'].astype(int)
     data_2v2['games'] = 1
     data_2v2 = data_2v2.groupby(['enemyComp']).agg({'win': 'sum', 'games': 'sum'})
-    data_2v2['winrate'] = round((data_2v2['win'] / data_2v2['games']) * 100, 1)
+    data_2v2['winrate'] = f"{round((data_2v2['win'] / data_2v2['games']) * 100, 1):.1f}"
     data_2v2 = data_2v2.sort_values(by=['winrate'], ascending=False)
     data_2v2 = data_2v2.reset_index()
     return data_2v2
