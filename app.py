72d3f932c964e4b89a312098b70810f18899b6e8
RanjeetPajeet <68920468+RanjeetPajeet@users.noreply.github.com>
2023-01-02 23:39:59 -0600

    Update app.py

--- app.py
+++ app.py
@@ -43,14 +43,22 @@ tab1, tab2, tab3 = st.tabs(["**2v2**", "**3v3**", "**5v5**"])
 
 with tab1:
     st.header("2v2 Data")
-    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
     if st.session_state.data_2v2 is not None:
         st.write(st.session_state.data_2v2[::-1])
+        spacer()
+        st.markdown("## Rating over time")
+        
 
 with tab2:
-   st.header("3v3 Data")
-   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+    st.header("3v3 Data")
+    if st.session_state.data_3v3 is not None:
+        st.write(st.session_state.data_3v3[::-1])
+        spacer()
+        st.markdown("## Rating over time")
 
 with tab3:
-   st.header("5v5 Data")
-   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+    st.header("5v5 Data")
+    if st.session_state.data_5v5 is not None:
+        st.write(st.session_state.data_5v5[::-1])
+        spacer()
+        st.markdown("## Rating over time")
