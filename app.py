a0fef53b74bcd815dd1f935a9490f8cbc818dc6a
RanjeetPajeet <68920468+RanjeetPajeet@users.noreply.github.com>
2023-01-02 23:37:52 -0600

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,15 @@ def spacer():
 if 'arena_data' not in st.session_state:
     st.session_state.arena_data = None
 
+if 'data_2v2' not in st.session_state:
+    st.session_state.data_2v2 = get_2v2_data()
+
+if 'data_3v3' not in st.session_state:
+    st.session_state.data_3v3 = get_3v3_data()
+
+if 'data_5v5' not in st.session_state:
+    st.session_state.data_5v5 = get_5v5_data()
+
     
 
 # ---------------------------------------------------------------------- #
@@ -33,8 +42,10 @@ spacer()
 tab1, tab2, tab3 = st.tabs(["**2v2**", "**3v3**", "**5v5**"])
 
 with tab1:
-   st.header("2v2 Data")
-   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+    st.header("2v2 Data")
+    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+    if st.session_state.data_2v2 is not None:
+        st.write(st.session_state.data_2v2[::-1])
 
 with tab2:
    st.header("3v3 Data")
