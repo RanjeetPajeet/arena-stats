a754596251bc13c0bb1317cf03c75f6e9835f337
RanjeetPajeet <68920468+RanjeetPajeet@users.noreply.github.com>
2023-01-20 17:20:23 -0600

    Update plots.py

--- plots.py
+++ plots.py
@@ -10,6 +10,8 @@ def plot_data(data: pd.DataFrame, is3v3: bool = False) -> alt.Chart:
 #         data = data.copy()
         data = data[11:]
         data = pd.concat([data[:102], data[125:]])
+#     else:
+#         data = pd.concat([data[:102], data[125:]])
     data["matchNum"] = [i+1 for i in range(len(data))]
     data["Match"] = data["matchNum"]        # added for better tooltip
     data["Rating"] = data["newTeamRating"]  # added for better tooltip
